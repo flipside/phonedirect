c38b697679ab13c60ea769b2604bf046445467db
Mat Tyndall <mat.tyndall@gmail.com>
2013-09-29 01:12:39 -0700

    added breadcrumbs

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
    $main = $('#main'),
    $search = $('#search'),
    $searchResults = $('#search-results'),
-   $att = $('#att')
+   $att = $('#att'),
+   $breadcrumb = $('#breadcrumb'),
+   $bca = $('#bc-active'),
+   $bc1 = $('#bc1'),
+   $bc2 = $('#bc2'),
+   $bc3 = $('#bc3'),
+   $bc4 = $('#bc4')
    ;
 
   // console.log('load');
@@ -23,6 +29,22 @@ $(document).ready(function() {
     evt.preventDefault();
     $search.toggleClass('hidden');
     $searchResults.toggleClass('hidden');
+    $breadcrumb.toggleClass('hidden');
+  });
+
+  $bca.on('click', function () {
+    $bc1.toggleClass('hidden');
+    $bca.appendTo($breadcrumb);
+    $bca.text($bc2.text());
+  });
+  $bc1.on('click', function () {
+    $bc2.toggleClass('hidden');
+    $bca.text($bc3.text());
+  });
+
+  $bc2.on('click', function () {
+    $bc3.toggleClass('hidden');
+    $bca.text($bc4.text());
   });
 
 });
