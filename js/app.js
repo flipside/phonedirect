0ab91f53383b30d6526b632fcac228f068dd837d
Mat Tyndall <mat.tyndall@gmail.com>
2013-09-29 15:36:31 -0700

    almost demo ready

--- js/app.js
+++ js/app.js
@@ -3,19 +3,38 @@ $(document).ready(function() {
    $jumbotron = $('.jumbotron'),
    $main = $('#main'),
    $search = $('#search'),
+   $popular = $('#popular'),
+   $recent = $('#recent'),
    $searchResults = $('#search-results'),
+   $userPhone = $('#user-phone'),
    $att = $('#att'),
+   $xfinity = $('#xfinity'),
    $breadcrumb = $('#breadcrumb'),
    $bca = $('#bc-active'),
    $bc1 = $('#bc1'),
    $bc2 = $('#bc2'),
    $bc3 = $('#bc3'),
-   $bc4 = $('#bc4')
+   $bc4 = $('#bc4'),
+   $mainmenu = $('#mainmenu'),
+   $payments = $('#payments'),
+   $phoneNumber = $('#phone-number'),
+   $paymentForm = $('#payment-form'),
+   $phoneForm = $('#phone-form'),
+   $xfinityWait = $('#xfinity-wait'),
+   $phoneWait = $('#phone-wait'),
+   $cancel = $('#cancel')
    ;
 
+  var phone = '';
   // console.log('load');
+  // start
   $start.on('click', function () {
     console.log('click');
+    phone = $phoneNumber.find('input').val();
+    console.log(phone);
+    $userPhone.text(phone);
+    $phoneWait.text(phone);
+    $phoneForm.val(phone);
     $jumbotron.toggleClass('hidden');
     $main.toggleClass('hidden');
   });
@@ -28,23 +47,70 @@ $(document).ready(function() {
   $att.on('click', function (evt) {
     evt.preventDefault();
     $search.toggleClass('hidden');
-    $searchResults.toggleClass('hidden');
+    $recent.toggleClass('hidden');
+    $popular.toggleClass('hidden');
+    // $searchResults.toggleClass('hidden');
     $breadcrumb.toggleClass('hidden');
+    $mainmenu.toggleClass('hidden');
   });
 
-  $bca.on('click', function () {
-    $bc1.toggleClass('hidden');
-    $bca.appendTo($breadcrumb);
-    $bca.text($bc2.text());
-  });
-  $bc1.on('click', function () {
+  $mainmenu.find('a').on('click', function (evt) {
+    evt.preventDefault();
+    // var curr = evt.currentTarget;
+    // console.log(evt.currentTarget.name);
     $bc2.toggleClass('hidden');
+    // $bca.appendTo($breadcrumb);
     $bca.text($bc3.text());
+    $mainmenu.toggleClass('hidden');
+    $payments.toggleClass('hidden');
+    // $phoneNumber.toggleClass('hidden');
   });
 
-  $bc2.on('click', function () {
+  $payments.find('a').on('click', function (evt) {
+    evt.preventDefault();
+    // var curr = evt.currentTarget;
+    // console.log(evt.currentTarget.name);
     $bc3.toggleClass('hidden');
     $bca.text($bc4.text());
+    $payments.toggleClass('hidden');
+    $paymentForm.toggleClass('hidden');
+    // $page3.toggleClass('hidden');
   });
+
+
+  $paymentForm.find('button').on('click', function (evt) {
+    evt.preventDefault();
+
+    setTimeout(function () {
+      alert("Your current balance is two hundred twenty six dollars and seventy two cents due by October twenty first twenty thirteen");
+
+    }, 5000);
+
+    $paymentForm.toggleClass('hidden');
+    $search.toggleClass('hidden');
+    $recent.toggleClass('hidden');
+    $popular.toggleClass('hidden');
+    $breadcrumb.toggleClass('hidden');
+
+  });
+
+  $xfinity.on('click', function (evt) {
+    evt.preventDefault();
+    $search.toggleClass('hidden');
+    $recent.toggleClass('hidden');
+    $popular.toggleClass('hidden');
+    // $breadcrumb.toggleClass('hidden');
+    $xfinityWait.toggleClass('hidden');
+  });
+
+  $cancel.on('click', function (evt) {
+    evt.preventDefault();
+    $search.toggleClass('hidden');
+    $recent.toggleClass('hidden');
+    $popular.toggleClass('hidden');
+    // $breadcrumb.toggleClass('hidden');
+    $xfinityWait.toggleClass('hidden');
+  });
+
 
 });
